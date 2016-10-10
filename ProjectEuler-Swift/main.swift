0f96b2cdb0eede9a73176922238a32cf68efa128
cruisediary <cruzdiary@gmail.com>
2016-10-10 10:29:07 +0900

    ADD Xcode Project

---
+++ ProjectEuler-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEuler-Swift
+//
+//  Created by CruzDiary on 10/10/2016.
+//  Copyright © 2016 swiftycruz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
