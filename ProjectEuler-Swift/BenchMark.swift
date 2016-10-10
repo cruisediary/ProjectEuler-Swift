b959f8dfbd2469084bbf2b62e421a8e1b38c1156
cruisediary <cruzdiary@gmail.com>
2016-10-10 12:39:47 +0900

    Add BenchMark

--- ProjectEuler-Swift/BenchMark.swift
+++ ProjectEuler-Swift/BenchMark.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import CoreFoundation
+
+public struct Benchmark {
+    
+    fileprivate let startTimeInterval: CFAbsoluteTime
+    fileprivate let key: String
+    fileprivate static var sharedInstance: Benchmark?
+    
+    public init(key: String) {
+        startTimeInterval = CFAbsoluteTimeGetCurrent()
+        self.key = key
+    }
+    
+    public func finish() {
+        let elapsed = CFAbsoluteTimeGetCurrent() - startTimeInterval
+        let formatedElapsed = String(format: "%.5f", elapsed)
+        print("\(key): \(formatedElapsed) sec.")
+    }
+    
+    public static func start(_ key: String = "Benchmark") {
+        sharedInstance = Benchmark(key: key)
+    }
+    
+    public static func finish() {
+        sharedInstance?.finish()
+        sharedInstance = nil
+    }
+    
+    public static func measure(_ key: String = "Benchmark", block: (Void) -> Void) {
+        let benchmark = Benchmark(key: key)
+        block()
+        benchmark.finish()
+    }
+}
+
+prefix operator ⏲
+public prefix func ⏲(handler: (Void) -> Void) {
+    Benchmark.measure(block: handler)
+}
