ba0179dd1308c1bd15de1081d7e9a3e13c953f98
cruisediary <cruzdiary@gmail.com>
2016-10-10 10:48:45 +0900

    ADD Problem protocol

---
+++ ProjectEuler-Swift/Problems/Problem.swift
@@ -0,0 +1,13 @@
+//
+//  Problem.swift
+//  ProjectEuler-Swift
+//
+//  Created by CruzDiary on 10/10/2016.
+//  Copyright © 2016 swiftycruz. All rights reserved.
+//
+
+import Foundation
+
+protocol Problem {
+    func answer() -> Int
+}
